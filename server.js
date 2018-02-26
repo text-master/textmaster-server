c082b431cafc0e118b8f5c9594e5b70ad9f26fef
Nam <namhoang.td09@gmail.com>
2018-02-26 15:04:13 +0700

    Update env

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const keywords = require('retext-keywords');
 const nlcstToString = require('nlcst-to-string');
 const { connect, connection } = require('./connection');
 const DbConnection = require('./db-connection');
+const PORT = process.env.PORT || 3000
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(PORT, () => {
+  console.log(`Listening on ${ PORT }`);
 });
 
 io.on('connection', async function(socket) {
